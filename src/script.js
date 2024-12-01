64cac6ff4da53744b95863bc2dc9e82b7a58ce9b
ziyandadavashe <ziyanda.davashe@yahoo.com>
2024-12-01 16:45:24 +0200

    Updated
    icons

--- src/script.js
+++ src/script.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "ff417f6e1553tb7e790a6334bb9cocff";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
